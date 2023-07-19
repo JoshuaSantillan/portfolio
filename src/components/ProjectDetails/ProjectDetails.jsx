50a5c769eeab21435ecd6f496516ac9b7f7f914f
Josh <jsantill@ucsd.edu>
2023-07-19 07:44:23 -0700

    Project details component arcitecture changed

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -3,33 +3,23 @@ import './ProjectDetails.css';
 export const projects = [
   {
     name: "Color Game",
-    id: 'color-game',
+    id: 'colorGame',
     tags: ['Javascript'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECT_HTML_0,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_0
-    ],
-    imageDescriptions: ['Test Caption One', 'Test Caption Two'],
-    videos: [],
-    videoDescriptions: [],
-    pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECTGL3_PDF_0],
-    pdfDescriptions: ['Project Description'],
-    summary: 'Fragment Shader',
   },
   {
     name: "Fragment Shader",
     id: 'fragment-shader',
     tags: ['OpenGL', 'C++'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_1,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_0,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_2
+    media: [
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_0, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_1, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_IMAGE_2, caption: '' },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_0, description: '', controls: false },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_1, description: '', controls: false },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_2, description: '', controls: false }
     ],
-    imageDescriptions: [],
-    videos: [process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_0, process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_1, process.env.REACT_APP_PORTFOLIO_PROJECTGL3_VIDEO_2],
-    videoDescriptions: [],
     pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECTGL3_PDF_0],
     pdfDescriptions: ['Project Description'],
     summary: 'Fragment Shader',
@@ -38,28 +28,25 @@ export const projects = [
     name: "Rigging & Animation",
     id: 'rigging-animation',
     tags: ['OpenGL', 'C++'],
-    iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_1,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_0,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_2
+    iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_0,
+    media: [
+      {type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_0, caption: 'Test1'},
+      {type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL5_VIDEO_0, description: 'Test2', controls: false},
+      {type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL5_IMAGE_1, caption: 'Test3'}
     ],
-    imageDescriptions: [],
     videos: [process.env.REACT_APP_PORTFOLIO_PROJECTGL5_VIDEO_0, process.env.REACT_APP_PORTFOLIO_PROJECTGL5_VIDEO_1, process.env.REACT_APP_PORTFOLIO_PROJECTGL5_VIDEO_2],
-    videoDescriptions: [],
     pdfs: [],
     pdfDescriptions: [''],
     summary: 'This Project Needs more Description',
   },
-  {
+   {
     name: "Particle System",
     id: 'particle-system',
     tags: ['Houdini'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI1_IMAGE_0,
-    images: [],
-    imageDescriptions: [],
-    videos: [process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI1_VIDEO_0],
-    videoDescriptions: ['This is a video of Particle System project'],
+    media: [
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI1_VIDEO_0, description: '', controls: false }
+    ],
     pdfs: [],
     pdfDescriptions: [],
     summary: 'Summary for FireWork particle simulator',
@@ -69,17 +56,14 @@ export const projects = [
     id: 'decarbonizing-clouds',
     tags: ['Research'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_0,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_2,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_3,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_4,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_5,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_6
+    media: [
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_1, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_2, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_3, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_4, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_5, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_IMG_6, caption: '' }
     ],
-    imageDescriptions: [],
-    videos: [],
-    videoDescriptions: [],
     pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_PDF_0, process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH3_PDF_1],
     pdfDescriptions: ['White Paper', 'Technical Description'],
     summary: 'This Project needs more detail',
@@ -89,56 +73,40 @@ export const projects = [
     id: 'cloth-simulation',
     tags: ['Houdini'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI2_IMAGE_0,
-    images: [],
-    imageDescriptions: [],
-    videos: [
-      process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI2_VIDEO_0,
-      process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI2_VIDEO_1
+    media: [
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI2_VIDEO_0, description: '', controls: false },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECT_HOUDINI2_VIDEO_1, description: '', controls: false }
     ],
-    videoDescriptions: ['This is video 1 of Cloth Simulation project', 'This is video 2 of Cloth Simulation project'],
     pdfs: [],
     pdfDescriptions: [],
     summary: 'Something about Spring Dampers and Aerodynamic Drag here',
   },
-  {
+   {
     name: "Subdivision",
     id: 'subdivision',
     tags: ['OpenGL', 'C++'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL4_IMAGE_1,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL4_IMAGE_0
-    ],
-    imageDescriptions: ['This is an image of Subdivision project'],
-    videos: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_0,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_2
-    ],
-    videoDescriptions: [
-      'This is video 1 of Subdivision project',
-      'This is video 2 of Subdivision project',
-      'This is video 3 of Subdivision project'
+    media: [
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL4_IMAGE_0, caption: '' },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_0, description: '', controls: false },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_1, description: '', controls: false },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL4_VIDEO_2, description: '', controls: false }
     ],
     pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECTGL4_PDF_0],
     pdfDescriptions: ['Project Write Up'],
     summary: 'Bezier Curves, Bspline Curves, and recursive sub-division curves',
-    videoControls: true,
   },
   {
     name: "Local Illumination",
     id: 'local-illumination',
     tags: ['OpenGL', 'C++'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_0,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_3,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_4,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_2,
+    media: [
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_3, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_4, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_1, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL1_IMAGE_2, caption: '' }
     ],
-    imageDescriptions: [
-      'Phong Model', 'View and Light Vectors'],
-    videos: [],
-    videoDescriptions: [],
     pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECTGL1_PDF_0],
     pdfDescriptions: ['Project Description'],
     summary: 'Local Illumination with Phong Shading using multiple lights',
@@ -148,17 +116,13 @@ export const projects = [
     id: 'shadow-mapping',
     tags: ['OpenGL', 'C++'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_0,
-    images: [
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_1,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_2,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_3,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_4,
-      process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_5,
+    media: [
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_1, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_2, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_3, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_4, caption: '' },
+      { type: 'image', src: process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_IMAGE_5, caption: '' }
     ],
-    imageDescriptions: [
-      'Test Caption One', 'Test Caption Two'],
-    videos: [],
-    videoDescriptions: [],
     pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_PDF0, process.env.REACT_APP_PORTFOLIO_PROJECTGL_SM_PDF1],
     pdfDescriptions: ['Final Write Up', 'Project Description'],
     summary: 'Extending Local illumination to have shadows',
@@ -169,15 +133,19 @@ export const projects = [
     summary: 'ERSP 2020 - Presentation',
     tags: ['Research', 'C++'],
     iconImage: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_IMAGE_0,
-    images: [],
-    imageDescriptions: ['This is an image of Network Simulator project'],
-    videos: [process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_VIDEO_0],
-    videoDescriptions: ['This is a video of Network Simulator project'],
-    pdfs: [process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_0, process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_1, process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_2],
+    media: [
+      { type: 'image', src: '', caption: '' },
+      { type: 'video', src: process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_VIDEO_0, description: '', controls: false }
+    ],
+    pdfs: [
+      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_0,
+      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_1,
+      process.env.REACT_APP_PORTFOLIO_PROJECT_RESEARCH1_PDF_2
+    ],
     pdfDescriptions: ["Motivation", "Proposal", "Final Poster"],
-    videoControls: true,
   }
 ];
+
 
 
 const ImageWithCaption = ({ src, caption }) => (
@@ -209,37 +177,60 @@ const PDFLink = ({ src, description }) => (
     </a>
   </div>
 );
+const checkMediaType = (project) => {
+  if (project.media && project.media.length > 0) {
+    return project.media.map((media, index) => {
+      if (media.type === 'image') {
+        return (
+          <div key={index}>
+            <p>{media.caption}</p>
+            <ImageWithCaption src={media.src}/>
+          </div>
+        );
+      } else if (media.type === 'video') {
+        if (media.description) {
+          return (
+            <div key={index}>
+              <p>{media.description}</p>
+              <VideoComponent src={media.src} controls={media.controls} />
+            </div>
+          );
+        } else {
+          return (
+            <div key={index}>
+              <p>{media.caption}</p>
+              <VideoComponent src={media.src} controls={media.controls} />
+            </div>
+          );
+        }
+      } else {
+        return null;
+      }
+    });
+  } else {
+    return null;
+  }
+};
 
 const ProjectDetails = ({ project }) => {
   return (
     <div className="project-details text-white">
       <h1>{project.name}</h1>
-
       <div className="project-summary">
         <p>{project.summary}</p>
       </div>
 
       <div className="project-media video-container">
-        {project.images && project.images.length > 0 && project.images.map((img, index) => (
-          <ImageWithCaption key={index} src={img} caption={project.imageDescriptions[index]} />
-        ))}
+        {checkMediaType(project)}
 
-
-        {project.videos && project.videos.length > 0 && project.videos.map((video, index) => (
-          <VideoComponent key={index} src={video} controls={project.videoControls} />
-        ))}
-
-
-
-        {project.pdfs && project.pdfs.length > 0 && project.pdfs.map((pdf, index) => (
+        {project.pdfs && project.pdfs.map((pdf, index) => (
           <PDFLink key={index} src={pdf} description={project.pdfDescriptions[index]} />
         ))}
-
-
       </div>
-
     </div>
+
   );
 };
+
 
 export default ProjectDetails;
