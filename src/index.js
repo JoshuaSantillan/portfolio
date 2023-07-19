987acd20b15a14037479138336bd7375d7da795a
Josh <jsantill@ucsd.edu>
2023-07-18 22:07:28 -0700

    Hash routing test GHpages

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
 
 reportWebVitals();
